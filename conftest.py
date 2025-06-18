c59fa96c30da1620ceb63db4c2e683c26946e6e4
Vladimir <anikeev30@yandex.ru>
2025-06-18 20:37:46 +0900

    изменено: task_manager/tasks/templates/tasks/task_list.html

--- conftest.py
+++ conftest.py
@@ -5,13 +5,9 @@ from task_manager.statuses.models import Status
 
 @pytest.fixture(autouse=True, scope="function")
 def setup_statuses(db):
-    print("Checking statuses...")
     if not Status.objects.exists():
-        print("Creating statuses...")
         Status.objects.bulk_create([
             Status(name="Новый"),
             Status(name="В работе"),
             Status(name="Завершен")
         ])
-    else:
-        print("Statuses already exist")
