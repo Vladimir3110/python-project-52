4aeff2a62c09302d7880be9645589f8dc3c60152
Vladimir <anikeev30@yandex.ru>
2025-07-07 00:28:29 +0900

    изменено: task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -150,8 +150,6 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
