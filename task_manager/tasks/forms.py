f4dceef466d8ef17bdfe2e62ff81c3f56391c154
Vladimir <anikeev30@yandex.ru>
2025-07-08 02:16:50 +0900

    изменено: task_manager/tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,55 +1,10 @@
-from django import forms
-from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+from django.forms import ModelForm
 
-from task_manager.statuses.models import Status
-
-from .models import Label, Task
-
-User = get_user_model()
+from .models import Task
 
 
-class TaskForm(forms.ModelForm):
-    status = forms.ModelChoiceField(
-        queryset=Status.objects.all(),
-        label=_("Status"),
-        widget=forms.Select(attrs={
-            "id": "id_status",
-            "class": "form-select",
-            "name": "status"
-        }),
-        required=True
-    )
-
-    assigned_to = forms.ModelChoiceField(
-        queryset=User.objects.all(),
-        label=_('Assigned to'),
-        required=False,
-        widget=forms.Select(attrs={'class': 'form-select'})
-    )
-
-    labels = forms.ModelMultipleChoiceField(
-        queryset=Label.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'form-select'}),
-        required=False,
-        label=_("Labels")
-    )
-
+class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assigned_to', 'labels']
-        labels = {
-            'name': _("Name"),
-            'description': _("Description"),
-            'assigned_to': _("Assignee"),
-        }
-        widgets = {
-            'assigned_to': forms.Select(attrs={'class': 'form-select'}),
-        }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['assigned_to'].queryset = User.objects.all()
-        self.fields['assigned_to'].label = _("Assigned to")
-
-        self.fields['status'].label = _("Status")
