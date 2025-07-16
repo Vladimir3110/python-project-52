86d02e5214d3a3474e88dbe5bb01768843fa5727
Vladimir <anikeev30@yandex.ru>
2025-07-16 21:26:50 +0900

    измененно: task_manager/tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,10 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.forms import ModelForm
 
 from .models import Task
+
+User = get_user_model()
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assigned_to', 'labels']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_to'].queryset = User.objects.all()
 
