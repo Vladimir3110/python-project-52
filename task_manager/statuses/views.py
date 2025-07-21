83072ead920ffe8ae9a1e86656e18c047e8ee4a5
Vladimir <anikeev30@yandex.ru>
2025-07-21 22:38:44 +0900

    измененно: task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,10 +1,13 @@
-from django.contrib import messages
+# from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import redirect
+
+# from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
+from task_manager.mixins import ProtectedDeleteMixin
 
 from .forms import StatusForm
 from .models import Status
@@ -32,25 +35,6 @@ class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     success_message = _("Status successfully updated")
 
 
-class StatusDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
-    model = Status
-    template_name = 'statuses/status_confirm_delete.html'
-    success_url = reverse_lazy('status_list')
-    success_message = _("Status successfully deleted")
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        
-        if self.object.tasks.exists(): 
-            messages.error(
-                request,
-                _("Cannot delete status in use"),
-                extra_tags='alert-danger'
-            )
-            return redirect(self.success_url)
-        
-        return super().delete(request, *args, **kwargs)
-
 # class StatusDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
 #    model = Status
 #    template_name = 'statuses/status_confirm_delete.html'
@@ -59,11 +43,27 @@ class StatusDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
 
 #    def delete(self, request, *args, **kwargs):
 #        self.object = self.get_object()
-#        if self.object.task_set.exists():
+        
+#        if self.object.tasks.exists(): 
 #            messages.error(
 #                request,
 #                _("Cannot delete status in use"),
 #                extra_tags='alert-danger'
 #            )
-#            return self.render_to_response(self.get_context_data())
+#            return redirect(self.success_url)
+        
 #        return super().delete(request, *args, **kwargs)
+
+class StatusDeleteView(ProtectedDeleteMixin, 
+                       SuccessMessageMixin, 
+                       LoginRequiredMixin, 
+                       DeleteView):
+    model = Status
+    template_name = 'statuses/status_confirm_delete.html'
+    success_url = reverse_lazy('status_list')
+    success_message = _("Status successfully deleted")
+    protected_message = _("Cannot delete status because it is in use")
+    protected_redirect = success_url
+
+    def check_protected_condition(self, obj):
+        return obj.tasks.exists()
