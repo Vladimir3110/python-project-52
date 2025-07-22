acd1a8e9f1bf0808a9e9243761a2c7ba4377c4ce
Vladimir <anikeev30@yandex.ru>
2025-07-22 22:07:08 +0900

    измененно: task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -157,6 +157,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WHITENOISE_IGNORE_MISSING_FILES = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
