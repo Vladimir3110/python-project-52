ea7bb9d9c02341c32510772c06501fbb63ed5267
Vladimir <anikeev30@yandex.ru>
2025-07-22 20:52:36 +0900

    измененно: build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install
-make migrate
+
+make install && make collectstatic && make migrate
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,8 +1,5 @@
-# from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-
-# from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
@@ -34,25 +31,6 @@ class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('status_list')
     success_message = _("Status successfully updated")
 
-
-# class StatusDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
-#    model = Status
-#    template_name = 'statuses/status_confirm_delete.html'
-#    success_url = reverse_lazy('status_list')
-#    success_message = _("Status successfully deleted")
-
-#    def delete(self, request, *args, **kwargs):
-#        self.object = self.get_object()
-        
-#        if self.object.tasks.exists(): 
-#            messages.error(
-#                request,
-#                _("Cannot delete status in use"),
-#                extra_tags='alert-danger'
-#            )
-#            return redirect(self.success_url)
-        
-#        return super().delete(request, *args, **kwargs)
 
 class StatusDeleteView(ProtectedDeleteMixin, 
                        SuccessMessageMixin, 
