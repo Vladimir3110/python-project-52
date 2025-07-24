2b0459021de5f41cbca90ea9fbecbe34ea1931a9
Vladimir <anikeev30@yandex.ru>
2025-07-24 21:01:07 +0900

    изменено: task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -151,7 +151,6 @@ LANGUAGES = [
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
