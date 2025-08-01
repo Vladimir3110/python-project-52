eb29a262529825555626d64cb60e52b54d960a37
Vladimir <anikeev30@yandex.ru>
2025-08-01 22:15:17 +0900

    изменено: task_manager/settings.py

--- task_manager/asgi.py
+++ task_manager/asgi.py
@@ -9,11 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.conf import settings
 from django.core.asgi import get_asgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
 application = get_asgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
