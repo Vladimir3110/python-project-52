c14f9a299905ca538da23afb7a6fb9d2e9483683
Vladimir <anikeev30@yandex.ru>
2025-08-06 22:45:29 +0900

    изменено: task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,12 +39,12 @@ ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', 'python-project-52-udhc.
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     "task_manager",
     'django_bootstrap5',
     'django_filters',
